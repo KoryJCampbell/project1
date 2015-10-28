61021156c2cb63040af07648ef46a2f10dd24f89
Kory Campbell <korycampbell@Dwights-MacBook-Pro.local>
2015-10-28 13:00:06 -0400

    Commit 1

---
+++ memory.js
@@ -0,0 +1,154 @@
+
+$(document).ready(function() {
+$(".button1").on("click", function() {
+    $("#myimage1").hide();
+    $(this).css('background', 'blue');
+    console.log("blue");
+  });
+})  ;
+
+$(document).ready(function() {
+  $(".button2").on("click", function() {
+    $("#myimage2").hide();
+    $(this).css('background', 'red');
+    console.log("red");
+  });
+});
+
+$(document).ready(function() {
+  $(".button3").on("click", function() {
+    $("#myimage3").hide();
+    $(this).css('background', 'yellow');
+    console.log("yellow");
+  });
+});
+
+$(document).ready(function() {
+  $(".button4").on("click", function() {
+    $("#myimage4").hide();
+    $(this).css('background', 'green');
+    console.log("green");
+  });
+});
+
+$(document).ready(function() {
+  $(".button5").on("click", function() {
+    $("#myimage5").hide();
+    $(this).css('background', 'purple');
+    console.log("purple");
+  });
+});
+
+$(document).ready(function() {
+  $(".button6").on("click", function() {
+    $("#myimage6").hide();
+    $(this).css('background', 'aqua');
+    console.log("aqua");
+  });
+});
+
+$(document).ready(function() {
+  $(".button7").on("click", function() {
+    $("#myimage7").hide();
+    $(this).css('background', 'pink');
+    console.log("pink");
+  });
+});
+
+$(document).ready(function() {
+  $(".button8").on("click", function() {
+    $("#myimage8").hide();
+    $(this).css('background', 'orange');
+    console.log("orange");
+  });
+});
+
+var button9 = $(document).ready(function() {
+$(".button9").on("click", function() {
+    $("#myimage9").hide();
+    $(this).css('background', 'blue');
+    console.log("blue");
+  });
+});
+
+$(document).ready(function() {
+  $(".button10").on("click", function() {
+    $("#myimage10").hide();
+    $(this).css('background', 'red');
+    console.log("red");
+  });
+});
+
+$(document).ready(function() {
+  $(".button11").on("click", function() {
+    $("#myimage11").hide();
+    $(this).css('background', 'yellow');
+    console.log("yellow");
+  });
+});
+
+$(document).ready(function() {
+  $(".button12").on("click", function() {
+    $("#myimage12").hide();
+    $(this).css('background', 'green');
+    console.log("green");
+  });
+});
+
+$(document).ready(function() {
+  $(".button13").on("click", function() {
+    $("#myimage13").hide();
+    $(this).css('background', 'purple');
+    console.log("purple");
+  });
+});
+
+$(document).ready(function() {
+  $(".button14").on("click", function() {
+    $("#myimage14").hide();
+    $(this).css('background', 'aqua');
+    console.log("aqua");
+  });
+});
+
+$(document).ready(function() {
+  $(".button15").on("click", function() {
+    $("#myimage15").hide();
+    $(this).css('background', 'pink');
+    console.log("pink");
+  });
+});
+
+$(document).ready(function() {
+  $(".button16").on("click", function() {
+    $("#myimage16").hide();
+    $(this).css('background', 'orange');
+    console.log("orange");
+  });
+});
+
+
+
+if($("button1") == ('white')){
+  alert("hello");
+}
+
+function matchCards(button1,button2){
+if(button1 != button2) {
+    alert("hello1");
+}
+}
+
+//PsudoCode For Matching Algorhythm
+
+/*
+
+if (button1's color us blue === button2's color is blue) {
+  alert = Match!
+function to fade away to white
+} else {
+  alert = Guess Again!
+function to reset cards
+}
+
+*/
